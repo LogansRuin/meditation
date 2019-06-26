86c4a264123d27b2aebf364b1f51cda23c55284c
Jared <jared@needapart.co.nz>
2019-06-27 10:43:08 +1200

    Install semantic-ui-react

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import { Header } from 'semantic-ui-react';
+
+class App extends React.Component {
+  render () {
+    return (
+      <Header as='h1'>React Dev has begun</Header>
+      )
+  }
+}
+
+export default App;
