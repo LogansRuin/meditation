bf11e8caf67dfdd0f268aae1509c642edc6f77ae
Jared <jared@needapart.co.nz>
2019-06-27 11:21:29 +1200

    Initial styling to the App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import '../App.css'
-import { Header } from 'semantic-ui-react';
+import AppHeader from './AppHeader';
+import { Container, Divider, Button, Grid, Header } from 'semantic-ui-react';
 
 class App extends React.Component {
   render () {
     return (
-      <Header as='h1'>React Dev has begun</Header>
-      )
+      <Container text textAlign="center">
+        <AppHeader/>
+        <Divider/>
+        <p>10:00</p>
+        <Button inverted secondary fluid>Start</Button>
+        <Grid columns = {2}>
+          <Grid.Column>
+            <Header as = "h2">Start</Header>
+            <Button inverted secondary icon="bell"/>
+          </Grid.Column>
+          <Grid.Column>
+            <Header as = "h2">End</Header>
+            <Button inverted secondary icon="bell"/>
+          </Grid.Column>
+        </Grid>
+      </Container>
+    )
   }
 }
 
---
+++ src/components/AppHeader.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Header } from 'semantic-ui-react';
+
+const AppHeader = () => {
+  return (
+    <Header as='h1'>Meditation time</Header>
+  )
+}
+
+export default AppHeader
